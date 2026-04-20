c76956504f66e17fc36107b420a137104d2509a1
Quentin Cyril Alaphilippe <qcyril-a@student.42lisboa.com>
2026-04-20 16:03:36 +0100

    refactor exit.c

--- src/execute/builtins/exit.c
+++ src/execute/builtins/exit.c
@@ -10,27 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minishell.h"
-
-static int	is_numeric(const char *s)
-{
-	int	i;
-
-	i = 0;
-	if (!s || !s[0])
-		return (0);
-	if (s[i] == '+' || s[i] == '-')
-		i++;
-	if (!s[i])
-		return (0);
-	while (s[i])
-	{
-		if (s[i] < '0' || s[i] > '9')
-			return (0);
-		i++;
-	}
-	return (1);
-}
+#include "../include/minishell.h"
 
 static void	clean_exit(t_shell *shell, int code)
 {
@@ -47,11 +27,51 @@ static void	exit_numeric_error(t_shell *shell, char *arg)
 	clean_exit(shell, 2);
 }
 
+static int	parse_ll(char *s, long long *out)
+{
+	int					i;
+	int					sign;
+	unsigned long long	n;
+
+	i = 0;
+	sign = 1;
+	n = 0;
+	if (!s || !s[0])
+		return (1);
+	if (s[i] == '+' || s[i] == '-')
+	{
+		if (s[i] == '-')
+			sign = -1;
+		i++;
+	}
+	if (!s[i])
+		return (1);
+	while (s[i])
+	{
+		if (s[i] < '0' || s[i] > '9')
+			return (1);
+		if (n > 922337203685477580ULL)
+			return (1);
+		n = n * 10ULL + (unsigned long long)(s[i] - '0');
+		if (sign == 1 && n > 9223372036854775807ULL)
+			return (1);
+		if (sign == -1 && n > 9223372036854775808ULL)
+			return (1);
+		i++;
+	}
+	if (sign == -1 && n == 9223372036854775808ULL)
+		*out = (long long)(-9223372036854775807LL - 1LL);
+	else
+		*out = (long long)(sign * (long long)n);
+	return (0);
+}
+
 int	ft_built_exit(char **av, t_shell *shell)
 {
-	int	code;
+	long long	code;
 
-	write(STDOUT_FILENO, "exit\n", 5);
+	if (isatty(STDIN_FILENO))
+		ft_putstr_fd("exit\n", 1);
 	if (!av[1])
 		clean_exit(shell, shell->exit_status);
 	if (av[2])
@@ -59,9 +79,8 @@ int	ft_built_exit(char **av, t_shell *shell)
 		ft_putstr_fd("minishell: exit: too many arguments\n", 2);
 		return (1);
 	}
-	if (!is_numeric(av[1]))
+	if (parse_ll(av[1], &code) != 0)
 		exit_numeric_error(shell, av[1]);
-	code = ft_atoi(av[1]) & 255;
-	clean_exit(shell, code);
+	clean_exit(shell, (unsigned char)code);
 	return (0);
 }
